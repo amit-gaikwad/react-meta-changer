d130282d27df6af38bf9162485809f9c36ec204e
Amit Gaikwad <amitgaikwad795@gmail.com>
2020-02-23 01:13:40 +0530

    fix useeffect error

--- src/components/MetaChanger/MetaChanger.js
+++ src/components/MetaChanger/MetaChanger.js
@@ -1,10 +1,8 @@
 import React, { useEffect } from "react";
 
 export const MetaChanger = props => {
-  useEffect(() => {
-    if (props.title) {
-      document.title = props.title;
-    }
-  }, []);
+  if (props.title) {
+    document.title = props.title;
+  }
   return <span></span>;
 };
